2f892d33ba0e5ece5ed28712ab4afd4596811e1f
Utkarsh Nigam <webxthedev@gmail.com>
2024-07-07 10:38:29 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -6,7 +6,7 @@ module.exports = async (request, response, next) => {
         const token = await request.headers.authorization.split(" ")[1];
         const decodedToken = await jwt.verify(
             token,
-            "RANDOM_TOKEN"
+            "RANDOM-TOKEN"
         );
         const user = await decodedToken;
         request.user=user;
